0821069592beef7d3f19aa20e6f4d91eeedb1982
Priyesh Singh <mrpriyesh037@gmail.com>
2024-12-21 11:28:36 +0530

    Create 183. Customers Who Never Order.sql

---
+++ LEETCODE/183. Customers Who Never Order.sql
@@ -0,0 +1,46 @@
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+code=
+wITH cu AS (
+    SELECT C.id
+    FROM Customers C
+    JOIN Orders O ON C.id = O.customer_id  
+)
+
+SELECT C.name as  Customers |
+FROM Customers C
+WHERE C.id NOT IN (SELECT id FROM cu);
