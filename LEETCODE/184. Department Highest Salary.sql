430b151916aa71df810a186a5168382febdd0335
Priyesh Singh <mrpriyesh037@gmail.com>
2024-12-21 14:19:24 +0530

    Create 184. Department Highest Salary.sql

---
+++ LEETCODE/184. Department Highest Salary.sql
@@ -0,0 +1,51 @@
+Write a solution to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
+code=
+select d.name as Department,
+e.name as Employee,
+e.salary as Salary
+from employee e
+join Department d
+on e.departmentid=d.id
+where (e.departmentid,e.salary) in(
+    select departmentid,max(salary)
+    From employee
+    where e.departmentid=d.id
+    group by departmentid
+)
